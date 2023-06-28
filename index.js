c106aafd90abba42611eea60cb670aa1652f0d3a
Breathe <704340916@qq.com>
2023-06-28 08:58:19 +0800

    a

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ const getFilesAndFoldersInDir = (path) => {
       })
     }
   })
+  // console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:result`, result)
   return result
 }
 
@@ -66,11 +67,12 @@ const cutImgs = async (folderName = '', options = {}) => {
   let elapsed = new Date().getTime()
   const imgDir = `${__dirname}/${folderName}` // 图片目录
   const outDri = `${__dirname}/${folderName}_out` // 图片输出目录
+  emptyDir(outDri) // 清空文件夹
 
   // 处理单张图片
-  const cutImg = async (name, index) => {
-    const filePath = `${imgDir}/${name}`
-    const newFilePath = `${outDri}/${name}`
+  const cutImg = async (name, { _imgDir, _outDri, index }) => {
+    const filePath = `${_imgDir}/${name}`
+    const newFilePath = `${_outDri}/${name}`
     console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:处理第[${index}]张图片${name}`, `=>`, newFilePath)
     const image = await Jimp.read(filePath)
     // image.quality(100) // 质量
@@ -79,30 +81,33 @@ const cutImgs = async (folderName = '', options = {}) => {
     // image.resize(3840, 1080) // 尺寸
     // image.cover(3840, 2160) // 尺寸
     cutFunc(image) // 加载传入的处理规则
-    await image.writeAsync(newFilePath)
+    image.writeAsync(newFilePath)
   }
 
-  emptyDir(outDri) // 清空文件夹
-  // 读取图片
-  const list_imgs = getFilesAndFoldersInDir(imgDir)
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:读取图片`, `${list_imgs.length}张`)
-  const _list_imgs = changeArrGroup(list_imgs, spliceNum)
-  let index = 0
-  // 分批次处理图片
-  for (const list of _list_imgs) {
-    const funcs = []
-    for (const item of list) {
-      index = index + 1
-      const func = cutImg(item.name, index)
-      funcs.push(func)
+  let index = 0 // 处理顺序
+  let funcs = [] // 汇总所有待处理图片
+  // 生成待处理图片方法
+  const initFuncs = async (_imgDir, _outDri) => {
+    const list_imgs = getFilesAndFoldersInDir(_imgDir) // 读取文件夹图片
+    for (const item of list_imgs) {
+      // 是文件夹并且存在子文件
+      if (item.type === 'folder' && item.children) {
+        _imgDir = `${_imgDir}/${item.name}`
+        _outDri = `${_outDri}/${item.name}`
+        await initFuncs(_imgDir, _outDri)
+      } else {
+        index = index + 1
+        const func = () => {
+          return cutImg(item.name, { index, _imgDir, _outDri })
+        }
+        funcs.push(func)
+      }
     }
-    await Promise.all(funcs)
   }
-  {
-    elapsed = new Date().getTime() - elapsed
-  }
-  const out_list_imgs = getFilesAndFoldersInDir(outDri)
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:处理完成,耗时${elapsed / 1000}s`, `${out_list_imgs.length}张`)
+  await initFuncs(imgDir, outDri)
+
+  elapsed = new Date().getTime() - elapsed // 总计耗时
+  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:处理完成,耗时${elapsed / 1000}s`, `${index}张`)
 }
 
 // 处理饥荒加载页面壁纸
@@ -112,7 +117,7 @@ const cutImgs = async (folderName = '', options = {}) => {
     image.crop(0, 0, 1820, 1024) // 剪切
     image.cover(1920, 1080) // 尺寸
   }
-  // cutImgs('dst_bg', { cutFunc })
+  cutImgs('dst_bg', { cutFunc })
 }
 
 // 处理steam的个人信息壁纸
@@ -121,5 +126,5 @@ const cutImgs = async (folderName = '', options = {}) => {
     image.quality(100) // 质量
     image.cover(1920, 1080) // 尺寸
   }
-  cutImgs('steam_bg', { cutFunc })
+  // cutImgs('steam_bg', { cutFunc })
 }
