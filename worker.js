3daad3142d45aa74fd6639ae7174e28e74ff2f05
Breathe <704340916@qq.com>
2025-03-05 20:13:11 +0800

    a

--- worker.js
+++
@@ -1,4 +0,0 @@
-const { index, func, parentPort } = require('worker_threads')
-
-// console.log(`Write-up on how ${workerData} wants to chill with the big boys`)
-parentPort.postMessage({ index, status: 'Done' })
